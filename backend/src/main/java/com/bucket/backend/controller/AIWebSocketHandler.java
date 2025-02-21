d774220055126a41b40ef82edbd7bbc143a81017
Yang Donggyun <ydg0724@gmail.com>
2025-02-21 22:01:45 +0900

    webSocket 전처리

--- backend/src/main/java/com/bucket/backend/controller/AIWebSocketHandler.java
+++ backend/src/main/java/com/bucket/backend/controller/AIWebSocketHandler.java
@@ -6,8 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 
 //AI 모델과 WebSocket으로 실시간으로 통신하는 핸들러
@@ -18,11 +20,32 @@ public class AIWebSocketHandler extends TextWebSocketHandler {
 
 
     //질문 답변받으면 추가할 예정
-
+    // session: websocket 세션
+    // message: json 메시지
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException{
 
-        //AI모델로 데이터 전송
-        client.sendToAI();
+        // Json 데이터를 Map 변환
+        Map<String, Object> rawData = objectMapper.readValue(message.getPayload(), Map.class);
+
+        // AI Input형태로 Json 변환
+        Map<String, Object> transformedData = convertJson(rawData);
+
+        // 변환된 데이터 AI로 전송
+        sendToAI(transformedData);
+    }
+
+    private Map<String,Object> convertJson(Map<String,Object> rawData){
+        Map<String, Object> aiInput = new HashMap<>();
+
+        return aiInput;
+    }
+
+    private void sendToAI(Map<String, Object> transformedData){
+        try{
+            client.sendToAI(objectMapper.writeValueAsString(transformedData));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
