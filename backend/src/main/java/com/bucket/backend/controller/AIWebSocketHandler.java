b9b82254f95e9a52db4f6e30e8cc42629191eb7f
Yang Donggyun <ydg0724@gmail.com>
2025-02-24 01:18:13 +0900

    필터링 추가

--- backend/src/main/java/com/bucket/backend/controller/AIWebSocketHandler.java
+++ backend/src/main/java/com/bucket/backend/controller/AIWebSocketHandler.java
@@ -1,6 +1,7 @@
 package com.bucket.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.websocket.Session;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.TextMessage;
@@ -17,8 +18,16 @@ import java.util.Map;
 @Service
 @Slf4j
 public class AIWebSocketHandler extends TextWebSocketHandler {
-    private ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private Session aiSession;
 
+    //사용할 관절 (필터링)
+    private final List<String> Points = Arrays.asList(
+            "Point_0", "Point_2", "Point_5", "Point_7", "Point_8",
+            "Point_11", "Point_12", "Point_13", "Point_14", "Point_15",
+            "Point_16", "Point_17", "Point_18", "Point_21", "Point_23",
+            "Point_25", "Point_26", "Point_28", "Point_30", "Point_31", "Point_32"
+    );
 
     public AIWebSocketHandler() {}
 
@@ -28,14 +37,20 @@ public class AIWebSocketHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException{
 
-        // Json 데이터를 Map 변환
-        Map<String, Object> rawData = objectMapper.readValue(message.getPayload(), Map.class);
+        try{
+            // Json 데이터를 Map 변환
+            Map<String, Object> rawData = objectMapper.readValue(message.getPayload(), Map.class);
+            log.info("Mediapipe 데이터 수신 : {}", rawData.toString());
 
-        // AI Input형태로 Json 변환
-        Map<String, Object> transformedData = convertJson(rawData);
+            // AI Input형태로 Json 변환
+            Map<String, Object> transformedData = convertJson(rawData);
 
-        // 변환된 데이터 AI로 전송
-        sendToAI(transformedData);
+            // 변환된 데이터 AI로 전송
+            sendToAI(transformedData);
+            
+        } catch (IOException e) {
+            log.error("WebSocket 메시지 처리 중 오류 발생", e);
+        }
     }
 
     private Map<String,Object> convertJson(Map<String,Object> rawData){
@@ -47,7 +62,7 @@ public class AIWebSocketHandler extends TextWebSocketHandler {
     private void sendToAI(Map<String, Object> transformedData){
         try{
             String json = objectMapper.writeValueAsString(transformedData);
-            
+
             log.info("AI 모델로 데이터 전송 완료: {}", json);
         }catch (IOException e){
             e.printStackTrace();
