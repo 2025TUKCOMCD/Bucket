de19603c7a42c5290c2662d9ba750ccd52e33632
Yang Donggyun <ydg0724@gmail.com>
2025-03-19 20:55:43 +0900

    회원가입 기능

--- backend/src/main/java/com/bucket/backend/controller/UserController.java
+++ backend/src/main/java/com/bucket/backend/controller/UserController.java
@@ -4,28 +4,40 @@ package com.bucket.backend.controller;
 import com.bucket.backend.model.users;
 import com.bucket.backend.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //유저 정보 관리 controller (회원가입, 로그인 등..)
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
+    private final UserService userService;
+
     public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     //회원가입 API
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody users user) {
 
-        return null;
+        String response = userService.registerUser(user);
+        if(response.equals("회원가입 성공!")) {
+            return ResponseEntity.ok(response);
+        }else{
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
     //로그인 API
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody users user) {
-
-        return null;
+    public ResponseEntity<String> login(@RequestParam String id, @RequestParam String password) {
+        String response = userService.loginUser(id, password);
+        if(response.equals("로그인 성공!")){
+            return ResponseEntity.ok(response);
+        }
+        else {
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
